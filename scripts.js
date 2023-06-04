dc9c7a1e87fdc2572760b7f85fb2e808188197e3
Perry <93749717+Tenlive1@users.noreply.github.com>
2023-06-03 21:16:49 -0500

    got the decimal to work

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
 const numscreen = document.querySelector('.number-screen');
 const equation  = document.querySelector('.equation'); 
-const numbers    = document.querySelectorAll('.number-button');
+const numbers   = document.querySelectorAll('.number-button');
 const symbols   = document.querySelectorAll('.symbol');
 const equal     = document.getElementById('equal');
 const clear     = document.querySelector('#clear');
 const back      = document.querySelector('#backspace');
 const sign      = document.querySelector('#pos-neg');
+const dec       = document.querySelector('#decimal');
 
 function math(){
     let num = "0";// this is current input
@@ -59,7 +60,7 @@ function math(){
             }
             else if(numA !== "" && num !== "" && !equation.textContent.includes("=")){// when they want to evaluate more than 2 number
                 numA = operation(numA,num,middle); 
-                numscreen.textContent = parseInt(numA);
+                numscreen.textContent = numA;
             }
             middle = symbutton.id;//middle will be whatever operation that user have clicked
             equation.textContent = numA + " " + middle;// this will display what user have clicked
@@ -71,18 +72,18 @@ function math(){
         if(middle === "" && numA === ""){// evaluating a single number
             numA = num;
             equation.textContent = numA + " = ";
-            numscreen.textContent = parseInt(numA);
+            numscreen.textContent = numA;
         }else if(middle !== "" && num === ""){
             equation.textContent = numA + " " + middle + " " + numscreen.textContent + " = ";
             numA = operation(numA,numscreen.textContent,middle);
             num = numscreen.textContent;
-            numscreen.textContent = parseInt(numA);
+            numscreen.textContent = numA;
             
         }
         else{//evaluating 2 number with an operation
             equation.textContent = numA + " " + middle + " " + num + " = ";
             numA = operation(numA,num,middle);
-            numscreen.textContent = parseInt(numA);
+            numscreen.textContent = numA;
         }
 
     });
@@ -104,19 +105,26 @@ function math(){
             numscreen.textContent = num;
         }
     });
+    dec.addEventListener('click',()=>{
+        if(!num.includes(".") || !numscreen.textContent.includes('.')){
+            num = num + ".";
+            numscreen.textContent = num;
+        }
+    });
     
 }
 
 function operation(numA,num,middle){// this will do all of the math so this way i don't have to copy and paste the if statement
+    
     if(middle == "+"){
-        numA = parseInt(numA) + parseInt(num);
+        numA =  parseFloat(numA) + parseFloat(num);
     }
     else if(middle == "-"){
-        numA = parseInt(numA) - parseInt(num);
+        numA = numA - num;
     }else if(middle == "*"){
-        numA = parseInt(numA) * parseInt(num);
+        numA = numA * num;
     }else if(middle == "/"){
-        numA = parseInt(numA) / parseInt(num);
+        numA = numA / num;
     }
     return numA;
 }
